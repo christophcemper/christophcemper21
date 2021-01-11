5955464e9b616db6cb43dbaf77f2820af0d06cfe
Christoph C. Cemper <christoph.cemper@linkresearchtools.org>
2021-01-11 21:41:34 +0100

    config fixes

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+USER=root
+HOST=smart.linkresearchtools.com
+DIR=var/www/html/christophcemper/   # the directory where your web site files should go
+
+hugo
+
+#list only
+#rsync -av --list-only --stats -e 'ssh -p 33899' --progress public/ ${USER}@${HOST}:/${DIR}
+
+#deploy
+
+cp config/.htaccess public/
+
+rsync -arv --stats -e 'ssh -p 33899' --progress --delete public/ ${USER}@${HOST}:/${DIR}
+
+exit 0
